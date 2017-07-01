88086191c6c0c060eaf82b9468c6e92260233642
Malo Skrylevo <majioa@yandex.ru>
2017-07-01 22:25:18 +0300

    Added date selection on pickitup

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,11 +1,12 @@
 class MemoriesController < ApplicationController
   before_action :set_memory, only: %i(show)
+  before_action :set_date, only: %i(index)
 
-  has_scope :by_date, only: %i(index)
-  has_scope :by_text, only: %i(index)
+  has_scope :with_date, only: %i(index)
+  has_scope :with_text, only: %i(index)
 
-  # GET /memories
-  # GET /memories.json
+  # GET /memories,/,/index
+  # GET /memories.js,/index.js
   def index
     @memories = apply_scopes(Memory).all.page(params[:page])
   end
@@ -16,9 +17,11 @@ class MemoriesController < ApplicationController
      render :show, locals: { locale: :ру }
   end
 
-  protected
+   protected
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_memory
-#     binding.pry
-    @memory = Memory.by_slug(params[:slug]).first&.decorate || raise(ActiveRecord::RecordNotFound) ;end;end
+   def set_date
+      @date = params[:with_date] || Date.today
+   end
+
+   def set_memory
+      @memory = Memory.by_slug(params[:slug]).first&.decorate || raise(ActiveRecord::RecordNotFound) ;end;end
--- app/webpack/js/behaviors/pickmeup.js
+++ app/webpack/js/behaviors/pickmeup.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
-  pickmeup('#calendar').show();
-  cal = $('.pickmeup').remove();
-  cal.removeAttr('style');
-  cal.appendTo('#calendar');
-});
+   pickmeup.defaults.locales['ру'] = {
+      days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      daysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
+   }
+   pickmeup('#calendar', {
+      locale: 'ру',
+      first_day: 0,
+   }).show()
+   cal = $('.pickmeup').remove()
+   cal.removeAttr('style')
+   cal.appendTo('#calendar')
+   $('#calendar').on('pickmeup-change', function (e) {
+      console.log("selected date", e.detail.date)
+      $('form#calendar-date input').attr('value', e.detail.formatted_date);
+      form = $('form#calendar-date');
+      valuesToSubmit = form.serialize();
+      request = {
+         url: form.attr('action') + '.js',
+         data: valuesToSubmit,
+         dataType: "html",
+         context: $('#page'),
+         global: false,
+         complete: function(_, state) {
+            console.log("/index.js:", state)
+         },
+         success: function(response) {
+            $('#page').replaceWith( "<div id='#page'>" + response + "</div>" )
+            history.pushState({ "html": response }, '', '/');
+         },
+      }
+
+      $.ajax(request)
+   })
+})
