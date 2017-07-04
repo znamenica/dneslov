d30425089c40c543fada4ce2d083e957a9585698
Malo Skrylevo <majioa@yandex.ru>
2017-07-05 01:28:45 +0300

    Отключён materialbox, исправлен index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require_tree .
 
 $(document).ready(function(){
-   $('.materialboxed').materialbox();
+   //$('.materialboxed').materialbox();
    $('.collapsible').collapsible();
    $('.carousel').carousel();
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615180100) do
+ActiveRecord::Schema.define(version: 20170703020600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,7 +103,7 @@ ActiveRecord::Schema.define(version: 20170615180100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "alphabeth_code"
-    t.string "info_type"
+    t.string "info_type", null: false
   end
 
   create_table "memoes", id: :serial, force: :cascade do |t|
@@ -189,7 +189,7 @@ ActiveRecord::Schema.define(version: 20170615180100) do
     t.string "author"
     t.string "source"
     t.string "description"
-    t.string "info_type"
+    t.string "info_type", null: false
     t.string "ref_title"
     t.integer "tone"
     t.index ["name", "alphabeth_code"], name: "index_services_on_name_and_alphabeth_code", unique: true
@@ -200,7 +200,7 @@ ActiveRecord::Schema.define(version: 20170615180100) do
     t.string "sluggable_type"
     t.integer "sluggable_id"
     t.index ["sluggable_type", "sluggable_id"], name: "index_slugs_on_sluggable_type_and_sluggable_id"
-    t.index ["text"], name: "index_slugs_on_text"
+    t.index ["text"], name: "index_slugs_on_text", unique: true
   end
 
 end
