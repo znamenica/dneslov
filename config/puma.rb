b3e3f37ee0be533a7dcf6b0a7db1f8b298f63fcf
Malo Skrylevo <majioa@yandex.ru>
2017-07-09 21:34:39 +0300

    Added puma conf

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
@@ -8,8 +8,21 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+if ENV['RACK_ENV'] == 'production'
+   app_dir = File.expand_path("../..", __FILE__)
+   shared_dir = "#{app_dir}/shared"
+   bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+   stdout_redirect "#{shared_dir}/tmp/log/stdout.log", "#{shared_dir}/tmp/log/stderr.log"
+   pidfile "#{shared_dir}/tmp/pids/puma.pid"
+   state_path "#{shared_dir}/pids/puma.state"
+   activate_control_app
+   daemonize true
+end
+
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+   # Worker specific setup for Rails 4.1+
+   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+   require "active_record"
+   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
