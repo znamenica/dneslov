17cb1c01cad4d64509e5b60e7593379b2a88854a
Malo Skrylevo <majioa@yandex.ru>
2017-07-11 02:03:41 +0300

    enable link to for secrets.yaml

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,7 @@ set :repo_url, "git@github.com:znamenica/dneslov.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
