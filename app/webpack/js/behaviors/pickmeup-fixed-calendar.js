4e478d403510b9eeb7e474bad858604120dac81b
Malo Skrylevo <majioa@yandex.ru>
2017-09-05 12:51:52 +0300

    Added support for next/prev days change

--- app/webpack/js/behaviors/pickmeup-fixed-calendar.js
+++ app/webpack/js/behaviors/pickmeup-fixed-calendar.js
@@ -1,4 +1,5 @@
-var pickmeup = require('pickmeup/dist/pickmeup.min.js')
+//var pickmeup = require('pickmeup/dist/pickmeup.min.js')
+var pickmeup = require('pickmeup/js/pickmeup.js')
 
 $(document).ready(function(){
    pickmeup.defaults.locales['ру'] = {
@@ -11,18 +12,40 @@ $(document).ready(function(){
    selected = new Date();
    selected.setTime(selected.getTime() + 9*60*60*1000);
    selected_string = selected.getDate() + "/" + (selected.getMonth() + 1) + "/" + selected.getFullYear();
-   pickmeup('#calendar', {
+   pmu = pickmeup('#calendar', {
       date: selected_string, // plus 9 hours
       today_offset: 8*60*60*1000,
       locale: 'ру',
       first_day: 0,
-   }).show()
+   })
+   pmu.show()
    cal = $('.pickmeup').remove()
    cal.removeAttr('style')
+   instance = cal.find('.pmu-instance')
+   nextprev = $('.next-prev').remove()
+   instance.append(nextprev)
    cal.appendTo('#calendar')
    $('#calendar').on('pickmeup-change', function (e) {
       console.log("selected date", e.detail.date)
       $('form#common-data input[name=with_date]').attr('value', e.detail.formatted_date);
+      filter_request()
+   })
+   $('#calendar .pmu-yesterday').on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      date = pmu.get_date();
+      date.setDate(date.getDate() - 1);
+      pmu.set_date(date);
+      $('form#common-data input[name=with_date]').attr('value', pmu.get_date(true));
+      filter_request()
+   })
+   $('#calendar .pmu-tomorrow').on('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      date = pmu.get_date();
+      date.setDate(date.getDate() + 1);
+      pmu.set_date(date);
+      $('form#common-data input[name=with_date]').attr('value', pmu.get_date(true));
       filter_request()
    })
 })
