a18bc6f323fe735aafe74d0a1c99b35152d8d8a0
Malo Skrylevo <majioa@yandex.ru>
2017-11-08 19:44:54 +0300

    Added languages support

--- app/components/matchers.jsx
+++ app/components/matchers.jsx
@@ -4,10 +4,93 @@ export function matchLetters(e) {
    if (e.text) {
       switch (e.alphabeth_code) {
       case 'ру':
-         res = ! e.text.match(/^[А-ЯЁа-яё:,.!;\-\/* ]+$/)
+         res = ! e.text.match(/^[А-ЯЁа-яё:,.!?;\-\/* ]+$/)
          break
       case 'цс':
          res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѼѾꙖꙊа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѽѿꙗꙋ:,.!;\-\/* ]+$/)
+      case 'рп':
+         res = ! e.text.match(/^[А-ЯЁІѢѲѴа-яёіѣѳѵ:,.!;\-\/* ]+$/)
+         break
+      case 'цр':
+         res = ! e.text.match(/^[А-ЯЁа-яё_<>:,.!;\-\/* ]+$/) //TODO
+         break
+      case 'сс':
+         res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѾꙖа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѿꙗ.,;* ]+$/) //TODO
+         break
+      case 'ук':
+         res = ! e.text.match(/^[А-ЩЬЮЯЄІЇҐа-щьюяєіїґ:,.!?\- ]+$/)
+         break
+      case 'бл':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'мк':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'ср':
+         res = ! e.text.match(/^[ЂЈ-ЋЏА-ИК-Шђј-ћа-ик-ш:,.!?\-\/ ]+$/)
+         break
+      case 'хр':
+         res = ! e.text.match(/^[A-PR-VZČĆŽĐŠa-pr-vzčćžđš:,. ]+$/) //TODO
+         break
+      case 'со':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'бг':
+         res = ! e.text.match(/^[А-ЪЬЮЯа-ъьюя:,.!?\-\/ ]+$/)
+         break
+      case 'чх':
+         res = ! e.text.match(/^[A-PR-VX-ZÁÉĚÍÓÚŮÝČĎŇŘŠŤŽa-pr-vx-záéěíóúůýčďňřšťž:,.!?\- ]+$/) //TOOD
+         break
+      case 'сл':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'по':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'ар':
+         res = ! e.text.match(/^[Ա-Ֆա-և:,.!?\- ]+$/)
+         break
+      case 'ив':
+         res = ! e.text.match(/^[ა-ჺჽა-ჺჽ:,.!?\- ]+$/)
+         break
+      case 'рм':
+         res = ! e.text.match(/^[A-ZĂÂÎȘȚa-zăâîșț:,.!?\- ]+$/)
+         break
+      case 'цу':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'гр':
+         res = ! e.text.match(/^[ͶͲΑ-ΫϏϒϓϔϘϚϜϠϞϴϷϹϺϾϿἈ-ἏἘ-ἝἨ-ἯἸ-ἿὈ-ὍὙ-ὟὨ-Ὧᾈ-ᾏᾘ-ᾟᾨ-ᾯᾸ-ᾼῈ-ῌῘ-ΊῨ-ῬῸ-ῼΩΆ-Ώά-ώϐϑϕ-ϗϙϛϝ-ϟϡ-ϳϵ-϶ϸϻϼᴦ-ᴪἀ-ἇἐ-ἕἠ-ἧἰ-ἷὀ-ὅὐ-ὗὠ-ὧὰ-ᾇᾐ-ᾗᾠ-ᾧᾰ-ᾷῂ-ῇῐ-ῗῠ-ῧῲ-ῷͻ-ͽͷΐά-ΰ:,. ]+$/)
+         break
+      case 'ла':
+         res = ! e.text.match(/^[A-IK-TVX-ZÆa-ik-tvx-zæ:,.\- ]+$/)
+         break
+      case 'ит':
+         res = ! e.text.match(/^[A-IL-VZa-il-vz:,.!?\- ]+$/)
+         break
+      case 'фр':
+         res = ! e.text.match(/^[A-ZŒÆÇÀÂÎÏÛÙÜÉÈÊËÔŸÑa-zœæçàâîïûùüéèêëôÿñ:,.!?\- ]+$/)
+         break
+      case 'ис':
+         res = ! e.text.match(/^[A-ZÑÁÉÍÓÚÜÏa-zñáéíóúüï:,. ]+$/)
+         break
+      case 'не':
+         res = ! e.text.match(/^[A-ZÄÖÜẞa-zäöüßſ:,. ]+$/)
+         break
+      case 'ир':
+         res = ! e.text.match(/^[A-IL-PR-Ua-il-pr-u:,. ]+$/)
+         break
+      case 'си':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'ан':
+         res = ! e.text.match(/^[A-Za-z:,.!?\- ]+$/)
+         break
+      case 'са':
+         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         break
+      case 'ра':
+         res = ! e.text.match(/^[A-IL-PR-UW-YÆÐꝽÞǷĊĠĀĒĪŌŪa-il-pr-uw-yæðᵹſþƿċġāēīūō:,.!?\- ]+$/) //TODO
          break
       }
    }
@@ -19,6 +102,31 @@ export function matchCodes(e) {
    const language_tree = {
       ру: ['рп', 'ру'],
       цс: ['цс', 'рп', 'ру', 'цр' ],
+      сс: ['сс'],
+      ук: ['ук'],
+      бл: ['бл'],
+      мк: ['мк'],
+      сх: ['ср', 'хр'],
+      со: ['со'],
+      бг: ['бг'],
+      чх: ['чх'],
+      сл: ['сл'],
+      по: ['по'],
+      кш: ['кш'],
+      вл: ['вл'],
+      нл: ['нл'],
+      ар: ['ар'],
+      ив: ['ив'],
+      рм: ['рм', 'цу', 'цр'],
+      гр: ['гр'],
+      ла: ['ла'],
+      ит: ['ит'],
+      фр: ['фр'],
+      ис: ['ис'],
+      не: ['не'],
+      ир: ['ир'],
+      си: ['си'],
+      ан: ['ан', 'са', 'ра'],
    }
    let alphabeth_codes = language_tree[e.language_code]
 
