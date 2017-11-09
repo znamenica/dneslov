5f4dd9ae3078deee79b11a8a94a14ccb3c83ba75
Malo Skrylevo <majioa@yandex.ru>
2017-11-09 17:40:40 +0300

    Fixed route counstraints

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
    get '/index' => 'memories#index'
 
    slug_options = { '/:slug' => 'memories#show', as: 'slug' }
-   slug_options[ :constraints ] = { slug: /[ёа-я0-9]{1,6}/ } if Rails.env.production? 
+   #slug_options[ :constraints ] = { slug: /[ёа-я0-9]{1,6}/ } if Rails.env.production? 
    get slug_options
   #
   # Example of regular route:
