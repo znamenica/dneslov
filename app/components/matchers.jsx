34b344c6753609cf2eeced1486f258be6960765a
Malo Skrylevo <majioa@yandex.ru>
2018-02-13 16:40:32 +0300

    Fixed allows chars crlf for text fields

--- app/components/matchers.jsx
+++ app/components/matchers.jsx
@@ -4,93 +4,93 @@ export function matchLetters(e) {
    if (e.text) {
       switch (e.alphabeth_code) {
       case 'ру':
-         res = ! e.text.match(/^[А-ЯЁа-яё:,.!?;\-\/*()0-9«»́– ]+$/)
+         res = ! e.text.match(/^[А-ЯЁа-яё:,.!?;\-\/*()0-9«»́–\r\n† ]+$/)
          break
       case 'цс':
-         res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѼѾꙖꙊа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѽѿꙗꙋ:,.!;\-\/* ]+$/)
+         res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѼѾꙖꙊа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѽѿꙗꙋ:,.!;\-\/*\r\n ]+$/)
       case 'рп':
-         res = ! e.text.match(/^[А-ЯЁІѢѲѴа-яёіѣѳѵ:,.!;\-\/*()0-9«»́– ]+$/)
+         res = ! e.text.match(/^[А-ЯЁІѢѲѴа-яёіѣѳѵ:,.!;\-\/*()0-9«»́–\r\n ]+$/)
          break
       case 'цр':
-         res = ! e.text.match(/^[А-ЯЁа-яё_<>:,.!;\-\/* ]+$/) //TODO
+         res = ! e.text.match(/^[А-ЯЁа-яё_<>:,.!;\-\/*\r\n ]+$/) //TODO
          break
       case 'сс':
-         res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѾꙖа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѿꙗ.,;* ]+$/) //TODO
+         res = ! e.text.match(/^[А-ЬЮЅІѠѢѦѮѰѲѴѶѸѺѾꙖа-ьюєѕіѡѣѧѯѱѳѵѷѹѻѿꙗ.,;*\r\n ]+$/) //TODO
          break
       case 'ук':
-         res = ! e.text.match(/^[А-ЩЬЮЯЄІЇҐа-щьюяєіїґ:,.!?\-()0-9́«»– ]+$/)
+         res = ! e.text.match(/^[А-ЩЬЮЯЄІЇҐа-щьюяєіїґ:,.!?\-()0-9́«»–\r\n ]+$/)
          break
       case 'бл':
-         res = ! e.text.match(/^[:,.()0-9«»– ]+$/) //TODO
+         res = ! e.text.match(/^[:,.()0-9«»–\r\n ]+$/) //TODO
          break
       case 'мк':
-         res = ! e.text.match(/^[:,.()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[:,.()0-9\r\n ]+$/) //TODO
          break
       case 'ср':
-         res = ! e.text.match(/^[ЂЈ-ЋЏА-ИК-Шђј-ћа-ик-ш:,.!?\-\/()0-9 ]+$/)
+         res = ! e.text.match(/^[ЂЈ-ЋЏА-ИК-Шђј-ћа-ик-ш:,.!?\-\/()0-9\r\n ]+$/)
          break
       case 'хр':
-         res = ! e.text.match(/^[A-PR-VZČĆŽĐŠa-pr-vzčćžđš:,.()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[A-PR-VZČĆŽĐŠa-pr-vzčćžđš:,.()0-9\r\n ]+$/) //TODO
          break
       case 'со':
-         res = ! e.text.match(/^[:,.()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[:,.()0-9\r\n ]+$/) //TODO
          break
       case 'бг':
-         res = ! e.text.match(/^[А-ЪЬЮЯа-ъьюя:,.!?\-\/()0-9 ]+$/)
+         res = ! e.text.match(/^[А-ЪЬЮЯа-ъьюя:,.!?\-\/()0-9\r\n ]+$/)
          break
       case 'чх':
-         res = ! e.text.match(/^[A-PR-VX-ZÁÉĚÍÓÚŮÝČĎŇŘŠŤŽa-pr-vx-záéěíóúůýčďňřšťž:,.!?\-()0-9 ]+$/) //TOOD
+         res = ! e.text.match(/^[A-PR-VX-ZÁÉĚÍÓÚŮÝČĎŇŘŠŤŽa-pr-vx-záéěíóúůýčďňřšťž:,.!?\-()0-9\r\n ]+$/) //TOOD
          break
       case 'сл':
-         res = ! e.text.match(/^[:,.()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[:,.()0-9\r\n ]+$/) //TODO
          break
       case 'по':
-         res = ! e.text.match(/^[:,.()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[:,.()0-9\r\n ]+$/) //TODO
          break
       case 'ар':
-         res = ! e.text.match(/^[Ա-Ֆա-և:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[Ա-Ֆա-և:,.!?\-()0-9\r\n ]+$/)
          break
       case 'ив':
-         res = ! e.text.match(/^[ა-ჺჽა-ჺჽ:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[ა-ჺჽა-ჺჽ:,.!?\-()0-9\r\n ]+$/)
          break
       case 'рм':
-         res = ! e.text.match(/^[A-ZĂÂÎȘȚa-zăâîșț:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[A-ZĂÂÎȘȚa-zăâîșț:,.!?\-()0-9\r\n ]+$/)
          break
       case 'цу':
-         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         res = ! e.text.match(/^[:,.\r\n ]+$/) //TODO
          break
       case 'гр':
-         res = ! e.text.match(/^[ͶͲΑ-ΫϏϒϓϔϘϚϜϠϞϴϷϹϺϾϿἈ-ἏἘ-ἝἨ-ἯἸ-ἿὈ-ὍὙ-ὟὨ-Ὧᾈ-ᾏᾘ-ᾟᾨ-ᾯᾸ-ᾼῈ-ῌῘ-ΊῨ-ῬῸ-ῼΩΆ-Ώά-ώϐϑϕ-ϗϙϛϝ-ϟϡ-ϳϵ-϶ϸϻϼᴦ-ᴪἀ-ἇἐ-ἕἠ-ἧἰ-ἷὀ-ὅὐ-ὗὠ-ὧὰ-ᾇᾐ-ᾗᾠ-ᾧᾰ-ᾷῂ-ῇῐ-ῗῠ-ῧῲ-ῷͻ-ͽͷΐά-ΰ:,.()0-9 ]+$/)
+         res = ! e.text.match(/^[ͶͲΑ-ΫϏϒϓϔϘϚϜϠϞϴϷϹϺϾϿἈ-ἏἘ-ἝἨ-ἯἸ-ἿὈ-ὍὙ-ὟὨ-Ὧᾈ-ᾏᾘ-ᾟᾨ-ᾯᾸ-ᾼῈ-ῌῘ-ΊῨ-ῬῸ-ῼΩΆ-Ώά-ώϐϑϕ-ϗϙϛϝ-ϟϡ-ϳϵ-϶ϸϻϼᴦ-ᴪἀ-ἇἐ-ἕἠ-ἧἰ-ἷὀ-ὅὐ-ὗὠ-ὧὰ-ᾇᾐ-ᾗᾠ-ᾧᾰ-ᾷῂ-ῇῐ-ῗῠ-ῧῲ-ῷͻ-ͽͷΐά-ΰ:,.()0-9\r\n ]+$/)
          break
       case 'ла':
-         res = ! e.text.match(/^[A-IK-TVX-ZÆa-ik-tvx-zæ:,.\- ]+$/)
+         res = ! e.text.match(/^[A-IK-TVX-ZÆa-ik-tvx-zæ:,.\-\r\n ]+$/)
          break
       case 'ит':
-         res = ! e.text.match(/^[A-IL-VZa-il-vz:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[A-IL-VZa-il-vz:,.!?\-()0-9\r\n ]+$/)
          break
       case 'фр':
-         res = ! e.text.match(/^[A-ZŒÆÇÀÂÎÏÛÙÜÉÈÊËÔŸÑa-zœæçàâîïûùüéèêëôÿñ:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[A-ZŒÆÇÀÂÎÏÛÙÜÉÈÊËÔŸÑa-zœæçàâîïûùüéèêëôÿñ:,.!?\-()0-9\r\n ]+$/)
          break
       case 'ис':
-         res = ! e.text.match(/^[A-ZÑÁÉÍÓÚÜÏa-zñáéíóúüï:,.()0-9 ]+$/)
+         res = ! e.text.match(/^[A-ZÑÁÉÍÓÚÜÏa-zñáéíóúüï:,.()0-9\r\n ]+$/)
          break
       case 'не':
-         res = ! e.text.match(/^[A-ZÄÖÜẞa-zäöüßſ:,.()0-9 ]+$/)
+         res = ! e.text.match(/^[A-ZÄÖÜẞa-zäöüßſ:,.()0-9\r\n ]+$/)
          break
       case 'ир':
-         res = ! e.text.match(/^[A-IL-PR-Ua-il-pr-u:,.()0-9 ]+$/)
+         res = ! e.text.match(/^[A-IL-PR-Ua-il-pr-u:,.()0-9\r\n ]+$/)
          break
       case 'си':
-         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         res = ! e.text.match(/^[:,.\r\n ]+$/) //TODO
          break
       case 'ан':
-         res = ! e.text.match(/^[A-Za-z:,.!?\-()0-9 ]+$/)
+         res = ! e.text.match(/^[A-Za-z:,.!?\-()0-9\r\n ]+$/)
          break
       case 'са':
-         res = ! e.text.match(/^[:,. ]+$/) //TODO
+         res = ! e.text.match(/^[:,.\r\n ]+$/) //TODO
          break
       case 'ра':
-         res = ! e.text.match(/^[A-IL-PR-UW-YÆÐꝽÞǷĊĠĀĒĪŌŪa-il-pr-uw-yæðᵹſþƿċġāēīūō:,.!?\-()0-9 ]+$/) //TODO
+         res = ! e.text.match(/^[A-IL-PR-UW-YÆÐꝽÞǷĊĠĀĒĪŌŪa-il-pr-uw-yæðᵹſþƿċġāēīūō:,.!?\-()0-9\r\n ]+$/) //TODO
          break
       }
    }
