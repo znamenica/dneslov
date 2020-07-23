1236add0ab8e4e40a8bd92297ae8d45e124a1c9f
Malo Skrylevo <majioa@yandex.ru>
2020-07-23 16:51:18 +0300

    Removed trash

--- 20171219154700_create_users.rb
+++
@@ -1,7 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[4.2]
-   def change
-      create_table :users do |t|
-         t.string :email
-         t.string :first_name
-         t.string :nick_name
-         t.string :last_name ;end;end;end
