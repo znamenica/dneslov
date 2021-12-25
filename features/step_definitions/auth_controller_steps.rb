02356f5634280d49cbaa6616fd6a0709b8545d2e
Pavel «Malo» Skrylev <majioa@yandex.ru>
2021-12-26 00:55:26 +0300

    fix: try fix redis run
    ! try fix redis run

--- features/step_definitions/auth_controller_steps.rb
+++ features/step_definitions/auth_controller_steps.rb
@@ -12,15 +12,19 @@
 Если("запросим короткие имена") do
    header 'Accept', 'application/json'
    header 'Content-Type', 'application/json'
-   @response = get('/short_names.json', session: { 'jwt': @token }) ;end
+   @response = get('/short_names.json', session: {'jwt': @token})
+end
 
 Если("сделаем {string} запрос к адресу {string} с параметром {string}") do |proto, address, parms|
-   @response = send( proto.downcase, address, parms ) ;end
+   @response = send(proto.downcase, address, parms)
+end
 
 То("получим вывод:") do |string|
    answer = JSON.parse(@response.body)
-   expect(answer.to_yaml.strip).to eq(string) ;end
+   expect(answer.to_yaml.strip).to eq(string)
+end
 
 То("заметим перенаправление на адрес {string} с ошибкою {string}") do |string, error|
    expect(@response.status).to eq(302)
-   expect(@response.body).to match(/dashboard\?error=#{error}/) ;end
+   expect(@response.body).to match(/dashboard\?error=#{error}/)
+end
