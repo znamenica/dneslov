e2962c2a37cb14bcda516c29b1c469bb055217e4
Pavel «Malo» Skrylev <majioa@yandex.ru>
2021-12-27 15:29:22 +0300

    ci: remove ci debugs
    - remove ci debugs

--- app/controllers/concerns/github_auth.rb
+++ app/controllers/concerns/github_auth.rb
@@ -2,13 +2,9 @@ module GithubAuth
    include ActiveSupport::Concern
 
    def github
-      Kernel.puts params.inspect
-      Kernel.puts params[:code].inspect
       authenticator = Auth::Github.new
-      # params = {}
       user_info = authenticator[params[:code]]
 
-      Kernel.puts user_info.inspect
       session_params = {
          'login': user_info['login'],
          'name': user_info['name'],
@@ -20,7 +16,6 @@ module GithubAuth
       }
 
       update_session(session_params)
-      Kernel.puts session_params.inspect
       logger.debug ("Session from Github: #{session.to_hash.inspect}")
 
       # ... create user if it doesn't exist...
@@ -31,11 +26,9 @@ module GithubAuth
       # ... and redirect to client app.
       new_params = session.to_hash.select {|x| %w(_csrf_token login name avatar_url location info).include?(x) }
 
-      Kernel.puts new_params.inspect
       redirect_to dashboard_path(new_params)
    rescue Exception => error
       Kernel.puts error.inspect
-      Kernel.puts dashboard_path(error: error.message)
       redirect_to dashboard_path(error: error.message)
    end
 
