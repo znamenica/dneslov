3100ec7dc06d3826485b07398e5ac7406b8d0c90
Pavel «Malo» Skrylev <majioa@yandex.ru>
2022-08-14 18:30:57 +0300

    fix: react-rails-ujs import in JS

--- app/webpack/js/about.js
+++ app/webpack/js/about.js
@@ -1,7 +1,7 @@
 // http://stackoverflow.com/a/30652110/873870
 function requireAll (r) { r.keys().forEach(r) }
 
-import { mountComponents } from 'react-rails-ujs/src/react-rails-ujs'
+import { mountComponents } from 'react-rails-ujs'
 
 import About from 'About'
 
--- app/webpack/js/admin.js
+++ app/webpack/js/admin.js
@@ -6,7 +6,7 @@ require('./behaviors/ext')
 // http://stackoverflow.com/a/30652110/873870
 function requireAll (r) { r.keys().forEach(r) }
 
-import { mountComponents } from 'react-rails-ujs/src/react-rails-ujs'
+import { mountComponents } from 'react-rails-ujs'
 
 import Dashboard from 'Dashboard'
 
