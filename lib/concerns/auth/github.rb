2887894bb9c94036f2a3cb10e0e751043e5f712e
Pavel «Malo» Skrylev <majioa@yandex.ru>
2023-10-31 02:05:17 +0300

    fix:  https requests on github auth
    ! fixed https requests on github auth

--- lib/concerns/auth/github.rb
+++ lib/concerns/auth/github.rb
@@ -1,10 +1,4 @@
 class Auth::Github
-   attr_reader :connection
-
-   def initialize connection = Excon
-      @connection = connection
-   end
-
    def [] code
       access_token_resp = fetch_github_access_token(code)
       access_token = access_token_resp['access_token']
@@ -23,7 +17,7 @@ class Auth::Github
       uri.query = URI.encode_www_form(query)
       res = Net::HTTP.get_response(uri)
 
-      if res.status != 200
+      unless res.is_a?(Net::HTTPOK)
          Rails.logger.error "GITHUB OAUTH ERROR: FETCH_ACCESS_TOKEN. Response headers are: #{resp.headers.inspect}"
          raise IOError, 'FETCH_ACCESS_TOKEN'
       end
@@ -32,15 +26,19 @@ class Auth::Github
    end
 
    def fetch_github_user_info access_token
-      req = Net::HTTP::Get.new(ENV["GITHUB_USER_INFO_URL"])
+      uri = URI(ENV["GITHUB_USER_INFO_URL"])
+      req = Net::HTTP::Get.new(uri)
       req['Authorization'] = "token #{access_token}"
       req['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 " +
          "(KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 
-      res = Net::HTTP.start(uri.hostname, uri.port) {|http| http.request(req) }
+      uri.query = URI.encode_www_form({})
+      http = Net::HTTP.new(uri.hostname, uri.port)
+      http.use_ssl = (uri.scheme == "https")
+      res = http.request(req)
 
-      if res.status != 200
-         Rails.logger.error "GITHUB OAUTH ERROR: FETCH_USER_INFO. Response headers are: #{resp.headers.inspect}"
+      unless res.is_a?(Net::HTTPOK)
+         Rails.logger.error "GITHUB OAUTH ERROR: FETCH_USER_INFO. Response headers are: #{res.instance_variable_get("@header").inspect}"
          raise IOError, 'FETCH_USER_INFO'
       end
 
